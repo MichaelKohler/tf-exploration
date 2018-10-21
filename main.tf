ae9210d69188e4e1232d7f44ab5d86e76233ef35
Michael Kohler <me@michaelkohler.info>
2018-10-21 15:18:49 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,72 @@
+terraform {
+  backend "s3" {
+    bucket = "mk-tfstate"
+    key    = "state_test"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = "default-key"
+  public_key = "${var.ssh_key}"
+}
+
+resource "aws_security_group" "tftest" {
+  name = "testsecgroup"
+  description = "Test Security Group"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_volume_attachment" "testebs_attach" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.testebs.id}"
+  instance_id = "${aws_instance.tftest.id}"
+}
+
+resource "aws_instance" "tftest" {
+  ami = "ami-0c0d066cb8b155cbe"
+  instance_type = "t3.nano"
+  key_name = "default-key"
+  security_groups = ["${aws_security_group.tftest.name}"]
+  tags {
+    Name = "tftest"
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 120; ansible-playbook -u admin -i '${self.public_ip},' test.yml"
+  }
+}
+
+resource "aws_ebs_volume" "testebs" {
+  availability_zone = "${var.aws_availability_zone}"
+  size = "${var.ebs_storage_size}"
+}
+
+resource "aws_eip" "lb" {
+  instance = "${aws_instance.tftest.id}"
+  vpc = true
+}
